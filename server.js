1d2c665c4e29efff8224c2cba1a2c895c39b4d5f
Joe Thor <joethor1989@gmail.com>
2018-10-04 20:42:34 -0500

    good good server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+//const routes = require("./routes");
+// var logger = require("morgan");
+var mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
+
+// Use morgan logger for logging requests
+// app.use(logger("dev"));
+
+//
+// Use body-parser for handling form submissions
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
@@ -12,8 +22,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/NYT";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Define API routes here
+// app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
