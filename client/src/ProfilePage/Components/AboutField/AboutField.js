e28300565cd7934ddf0e9e51667ebccce8a10298
JZStudent <joshzepeda.student@gmail.com>
2018-10-06 13:54:10 -0500

    updated

---
+++ client/src/ProfilePage/Components/AboutField/AboutField.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Container1 extends React.Component {
+  render() {
+    return
+
+    // Input Form
+    // Submit
+  }
+}
+
+export default Container1;
---
+++ client/src/ProfilePage/Components/AboutMe/AboutMe.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container1 from "./Container1";
+import Container2 from "./Container2";
+
+
+const About = () => (
+    <div>
+        <img src="../../Images/chickenpotpie1.jpg" />
+    
+    <AboutField style={{  }}>
+
+    </AboutField>
+    </div>
+);
+
