7a1ed20a2a70d11e375620322f2d69a193beeef7
jonwells10 <jonathonwells10@gmail.com>
2018-10-10 23:30:58 -0500

    cleaning up code changing state inputs

--- client/src/pages/Recipe.js
+++ client/src/pages/Recipe.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import Card from "../components/Card";
-import Alert from "../components/Alert";
 
-class Discover extends Component {
+
+class Recipe extends Component {
   state = {
+    _id: "",
+    user: "",
+    recipeName: "",
     image: "",
-    match: false,
-    matchCount: 0
+    description: "",
+    indgredients: "",
+    primaryMeat: "",
+    count: 0
   };
 
   // When the component mounts, load the next dog to be displayed
@@ -23,16 +28,12 @@ class Discover extends Component {
     const newState = { ...this.state };
 
     if (btnType === "pick") {
-      // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-      newState.match = 1 === Math.floor(Math.random() * 5) + 1;
-
-      // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-      newState.matchCount = newState.match
-        ? newState.matchCount + 1
-        : newState.matchCount;
+      // Update recipe count depending on wether or not the user likes the meat
+      newState.count = + 1;
+      
     } else {
-      // If we thumbs down'ed the dog, we haven't matched with it
-      newState.match = false;
+      // If we thumbs down'ed the Meat, we haven't matched with it
+      newState.count = - 1;
     }
     // Replace our component's state with newState, load the next recipe image
     this.setState(newState);
@@ -60,12 +61,12 @@ class Discover extends Component {
         <h1 className="text-center">
           You've liked {this.state.matchCount} meats!
         </h1>
-        <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">
-          Meat!
-        </Alert>
+        <h1>
+          These are the Top 5 Meatcipes!
+        </h1>
       </div>
     );
   }
 }
 
-export default Discover;
+export default Recipe
