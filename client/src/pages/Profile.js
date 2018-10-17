16eed3438bd4acc8abad616d085ae340e5d222b7
Joe Thor <joethor1989@gmail.com>
2018-10-17 13:07:36 -0500

    updated Profile.js to fix the import routes for aboutMe, myrecipe, and imageloader. They were going a level too deep.

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import AboutMe from "../components/AboutMe";
+import MyRecipe from "../components/MyRecipe";
+import ImageLoader from "../components/ImageLoader";
+import "./Profile.css"
+
+class Profile extends Component {
+    
+    state={
+        aboutText: '',
+        descriptionText: ''
+    }
+
+    handleAboutChange = data => {
+        this.setState({ aboutText: data })
+        console.log(data);
+    }
+
+    handleDescriptionChange = data => {
+        this.setState({ descriptionText: data })
+    }
+
+    render() {
+        return (
+            <div>
+            <div>
+                <ImageLoader></ImageLoader>
+                <AboutMe handleAboutChange={this.handleAboutChange}></AboutMe>
+                <MyRecipe handleDescriptionChange={this.handleDescriptionChange}></MyRecipe>
+            </div>
+            <div className="content">
+                { this.state.aboutText }
+            </div> 
+            <div className="content">
+                { this.state.descriptionText } 
+            </div>
+            </div>
+        )
+    }
+
+};
+
+export default Profile;
